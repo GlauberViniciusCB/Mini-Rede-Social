8dcf6158c25b8ac53d3939dd33ada0cbc8bdc917
GlauberViniciusCB <g_viniciuscb@outlook.com>
2025-05-11 11:55:20 -0300

    Criação da Classe Usuario

--- src/main/java/MiniRedeSocial/bean/entity/Usuario.java
+++ src/main/java/MiniRedeSocial/bean/entity/Usuario.java
@@ -1,5 +1,28 @@
 package MiniRedeSocial.bean.entity;
 
-public class Usuario {
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Table(name="USUARIO")
+@Entity
+public class Usuario {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "USUARIO_ID")
+    private  long id; 
+    
+    @Column(name = "USUARIO_NOME", nullable = false,length = 50)
+    private String nomeUsuario;
+
+    @Column(name = "USUARIO_EMAIL", nullable = false)
+    private String emailUsuario;
+
+    @Column(name = "USUARIO_SENHA", nullable = false )
+    private String senhaUsuario;
 }
