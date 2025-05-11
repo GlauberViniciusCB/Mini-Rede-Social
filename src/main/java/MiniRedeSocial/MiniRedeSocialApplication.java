32c119ab16ec97dc48a2c68e11b10f3fc8785fcc
GlauberViniciusCB <g_viniciuscb@outlook.com>
2025-05-11 11:06:59 -0300

    Mini Rede Social

---
+++ src/main/java/MiniRedeSocial/MiniRedeSocialApplication.java
@@ -0,0 +1,13 @@
+package MiniRedeSocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniRedeSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniRedeSocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MiniRedeSocial/MiniRedeSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package MiniRedeSocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniRedeSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
